5ba5e5551a5e44b3b4b6404874667bd79594b151
Fad-ikram <if.benyelles@esi-sba.dz>
2025-07-31 12:38:06 +0100

    some changes

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,4 +1,4 @@
-import TodoCard from "../../components/dashboard/TodoCard";
+import TodoCard from "../../components/dashboard/ToDoCard";
 import TodoStats from "../../components/dashboard/TodoStats";
 import DropDownMenu from "../../components/Global/DropDownMenu";
 import Model from "../../components/Global/Model";
